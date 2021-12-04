9bdfe54fa24f6e46be991f40a1878fdc55636527
Ella <rafaela.morato1805@gmail.com>
2021-12-04 23:19:45 +1000

    api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const commentRouter = require('./comment-routes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/comments', commentRouter);
+router.use('/users', userRoutes);
+router.use('./post', postRoutes);
+
+module.exports = router;
