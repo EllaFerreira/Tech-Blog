82d3e5a0c8279aead2e5b3802a1228019de5ac86
Ella <rafaela.morato1805@gmail.com>
2021-12-12 21:18:07 +1000

    testing db

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,14 @@
 const sequelize = require('../config/connection');
 const userSeed = require('./user-seeds');
-const commentsSeed = require('./comment-seeds');
-const postSeed = require('./post-seeds');
+const seedComments = require("./comment-seeds");
+const seedPosts = require("./post-seeds");
 
 const seedDatabase = async() => {
     await sequelize.sync({ force: true });
 
     await userSeed();
-    await commentsSeed();
-    await postSeed();
+    await seedComments();
+    await seedPosts();
     process.exit(0)
 }
 seedDatabase();
