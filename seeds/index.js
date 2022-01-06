2451708839074b69ebfa8559e37bbe91a6302601
Ella <rafaela.morato1805@gmail.com>
2022-01-06 19:13:49 +1000

    bug

--- seeds/index.js
+++ seeds/index.js
@@ -4,11 +4,15 @@ const seedComments = require("./comment-seeds");
 const seedPosts = require("./post-seeds");
 
 const seedDatabase = async() => {
+
+    // await sequelize.query("SET FOREIGN_KEY_CHECKS=0;")
+
     await sequelize.sync({ force: true });
 
     await userSeed();
-    await seedComments();
     await seedPosts();
+    await seedComments();
+    // await sequelize.query("SET FOREIGN_KEY_CHECKS=1;");
     process.exit(0)
 }
 seedDatabase();
